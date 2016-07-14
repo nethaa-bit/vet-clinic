3b54bd1da2205f92f8d9226c297f4bb6236f7820
Priya <>
2016-07-14 23:15:51 +0800

    Created ui login and domain of customer and pet

---
+++ src/domain/Customer.java
@@ -0,0 +1,63 @@
+
+package domain;
+
+public class Customer {
+    private String custIC;
+    private String custName;
+    private char custGender;
+    private String custAddress;
+    private String custPhoneNum;
+
+    public Customer() {
+    }
+
+    public Customer(String custIC, String custName, char custGender, String custAddress, String custPhoneNum) {
+        this.custIC = custIC;
+        this.custName = custName;
+        this.custGender = custGender;
+        this.custAddress = custAddress;
+        this.custPhoneNum = custPhoneNum;
+    }
+
+    public String getCustIC() {
+        return custIC;
+    }
+
+    public void setCustIC(String custIC) {
+        this.custIC = custIC;
+    }
+
+    public String getCustName() {
+        return custName;
+    }
+
+    public void setCustName(String custName) {
+        this.custName = custName;
+    }
+
+    public char getCustGender() {
+        return custGender;
+    }
+
+    public void setCustGender(char custGender) {
+        this.custGender = custGender;
+    }
+
+    public String getCustAddress() {
+        return custAddress;
+    }
+
+    public void setCustAddress(String custAddress) {
+        this.custAddress = custAddress;
+    }
+
+    public String getCustPhoneNum() {
+        return custPhoneNum;
+    }
+
+    public void setCustPhoneNum(String custPhoneNum) {
+        this.custPhoneNum = custPhoneNum;
+    }
+    
+    
+}
---
+++ src/domain/Pet.java
@@ -0,0 +1,122 @@
+
+package domain;
+
+import java.util.Date;
+
+
+public class Pet {
+   private String petID;
+   private String petName;
+   private Double petHeight;
+   private Double petWeight;
+   private Double petLength;
+   private String animalType;
+   private String breed;
+   private Date petDOB;
+   private char petGender;
+   private Customer customer;
+
+    public Pet() {
+    }
+
+    public Pet(String petID, String petName, Double petHeight, Double petWeight, Double petLength, String animalType, String breed, Date petDOB, char petGender, Customer customer) {
+        this.petID = petID;
+        this.petName = petName;
+        this.petHeight = petHeight;
+        this.petWeight = petWeight;
+        this.petLength = petLength;
+        this.animalType = animalType;
+        this.breed = breed;
+        this.petDOB = petDOB;
+        this.petGender = petGender;
+        this.customer = customer;
+    }
+
+    public String getPetID() {
+        return petID;
+    }
+
+    public void setPetID(String petID) {
+        this.petID = petID;
+    }
+
+    public String getPetName() {
+        return petName;
+    }
+
+    public void setPetName(String petName) {
+        this.petName = petName;
+    }
+
+    public Double getPetHeight() {
+        return petHeight;
+    }
+
+    public void setPetHeight(Double petHeight) {
+        this.petHeight = petHeight;
+    }
+
+    public Double getPetWeight() {
+        return petWeight;
+    }
+
+    public void setPetWeight(Double petWeight) {
+        this.petWeight = petWeight;
+    }
+
+    public Double getPetLength() {
+        return petLength;
+    }
+
+    public void setPetLength(Double petLength) {
+        this.petLength = petLength;
+    }
+
+    public String getAnimalType() {
+        return animalType;
+    }
+
+    public void setAnimalType(String animalType) {
+        this.animalType = animalType;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public Date getPetDOB() {
+        return petDOB;
+    }
+
+    public void setPetDOB(Date petDOB) {
+        this.petDOB = petDOB;
+    }
+
+    public char getPetGender() {
+        return petGender;
+    }
+
+    public void setPetGender(char petGender) {
+        this.petGender = petGender;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public String toString() {
+        return "Pet{" + "petID=" + petID + ", petName=" + petName + ", petHeight=" + petHeight + ", petWeight=" + petWeight + ", petLength=" + petLength + ", animalType=" + animalType + ", breed=" + breed + ", petDOB=" + petDOB + ", petGender=" + petGender + ", customer=" + customer + '}';
+    }
+
+
+  
+}
